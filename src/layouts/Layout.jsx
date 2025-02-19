a1d615e6c3b5a8ecbf6a9bdf79859f1c2fe0f7f4
amina5789 <merzamedovaa@gmail.com>
2025-02-19 16:42:48 +0300

    feature:footer

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import { Footer } from "../components/Footer/Footer";
 
 const Layout = () => {
   return (
@@ -7,7 +8,7 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
-      <p>footer</p>
+      <Footer/>
     </>
   );
 };
