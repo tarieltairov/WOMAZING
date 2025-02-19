f25fd754d41e83ce1affd4000dae0b97e6620353
Umarali <umaraliomorov13@gmail.com>
2025-02-19 23:41:32 +0600

    fix:callback_modal added modal_success

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,12 @@
+import { CallbackModal } from "../../components/CallbackModal";
+import { CallbackModalSuccess } from "../../components/CallbackModalSuccess/CallbackModalSuccess";
 import { Hero } from "../../components/Hero";
 
 function Main() {
   return (
     <>
+      {/* <CallbackModal /> */}
+      {/* <CallbackModalSuccess /> */}
       <Hero />
       <section>new collection</section>
       <section>important for us</section>
