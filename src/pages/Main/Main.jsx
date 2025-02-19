c6bf63db56cc9c171e402e09abb4530bb902ee3f
nadin9999 <nadinanarbekova8@gmail.com>
2025-02-19 19:58:53 +0600

    feature: new collection

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,15 @@
 import { Hero } from "../../components/Hero";
+import { NewCollection } from "../../components/Template/NewCollection";
 
 function Main() {
   return (
     <>
       <Hero />
       <section>new collection</section>
+      <section>hero</section>
+      <section>
+      <NewCollection/>
+      </section>
       <section>important for us</section>
       <section>team</section>
     </>
