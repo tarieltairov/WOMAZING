10cf581ef1f63c27b9b5d731ef0112c87c0eb7c1
Tariel <tarieltairov1@gmail.com>
2025-02-21 15:15:44 +0600

    fix: Main

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,14 +1,11 @@
 import { Hero } from "../../components/Hero";
-import { NewCollection } from "../../components/Template/NewCollection";
 import { NewCollection } from "../../components/NewCollection";
 
 function Main() {
   return (
     <>
       <Hero />
-      <section>new collection</section>
-      <section>hero</section>
-      <NewCollection/>
+      <NewCollection />
       <section>important for us</section>
       <section>team</section>
     </>
