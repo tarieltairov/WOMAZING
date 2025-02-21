c5832d0b0d643a3ef311cd9ffb8842b94b0a950a
Tariel <tarieltairov1@gmail.com>
2025-02-21 15:31:53 +0600

    fix: minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/routes";
 import "./styles/global.scss";
-import Cart from ".";
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={routes} />
-    </>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
