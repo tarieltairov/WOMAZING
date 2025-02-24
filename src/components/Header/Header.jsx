15c2a56d36af7d5ec57cc418abbef41390b1bb15
roza123-coder <kabylovaroza40@gmail.com>
2025-02-24 20:46:33 +0600

    Feat/header modified styles (#23)
    * added header
    
    * feat: fix comments
    
    * feat: delete font-family
    
    * changed Header and LogoNav styles
    
    * changed Header and LogoNav styles
    
    * feat: modified styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import phonenumber from "../../assets/icons/phone.svg";
 import cart from "../../assets/icons/cart.svg";
 import styles from "./Header.module.scss";
 import PhoneIcon from "./PhoneIcon";
-import { useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { ROUTER_PATHS } from "../../routes/routesPaths";
 import { AppContainer } from "../../layouts/AppContainer";
 import { LogoNav } from "../LogoNav";
@@ -13,20 +12,21 @@ export function Header() {
   const [cartCount, setCartCount] = useState(3);
 
   return (
-    <AppContainer className={styles.header}>
-      <div className={styles.wrapper}>
+    <AppContainer>
+      <header className={styles.header}>
         <LogoNav />
 
-        <div className={styles.contactContainer}>
-          <div className={styles.phoneIconContainer}>
-            <PhoneIcon className={styles.phoneIcon} />
+        <div className={styles.contact}>
+          <div className={styles.phoneWrapper}>
+            <div className={styles.phone}>
+              <PhoneIcon className={styles.phoneIcon} />
+            </div>
+            <div className={styles.tell}>
+              <a href="tel:+74958235412" className={styles.a}>
+                +7 (495) 823-54-12
+              </a>
+            </div>
           </div>
-
-          <img
-            src={phonenumber}
-            alt="phone-img"
-            className={styles.phoneNumberImg}
-          />
           <div
             className={styles.cartContainer}
             onClick={() => navigate(ROUTER_PATHS.cart)}
@@ -35,7 +35,7 @@ export function Header() {
             {cartCount && <span className={styles.cartBadge}>{cartCount}</span>}
           </div>
         </div>
-      </div>
+      </header>
     </AppContainer>
   );
 }
--- src/components/LogoNav/LogoNav.jsx
+++ src/components/LogoNav/LogoNav.jsx
@@ -14,18 +14,19 @@ const menuItems = [
 
 export const LogoNav = () => {
   return (
-    <div className={styles.logoNavContainer}>
-      <Link className={styles.logoContainer} to={ROUTER_PATHS.main}>
+    <div className={styles.root}>
+      <Link className={styles.logo} to={ROUTER_PATHS.main}>
         <img src={logo} alt="logo" />
-        <img src={Womazing} alt="Womazing" />
+        <div className={styles.logoTitle}>Womazing</div>
       </Link>
+
       <nav>
-        <ul className={styles.menu}>
+        <ul className={styles.navigation}>
           {menuItems.map((item, index) => (
             <li key={index}>
               <NavLink
                 to={item.path}
-                className={({ isActive }) => (isActive ? styles.active : "")}
+                className={({ isActive }) => (isActive ? styles.o : "")}
               >
                 {item.name}
               </NavLink>
