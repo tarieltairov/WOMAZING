ccab8e386b9593fee5fdf3da5a09f870bd600848
Tariel <tarieltairov1@gmail.com>
2025-02-24 21:40:34 +0600

    added Important

---
+++ src/components/Important/Important.jsx
@@ -0,0 +1,39 @@
+import Quality from "../../assets/icons/Frame 16.svg";
+import Speed from "../../assets/icons/Frame 16 (1).svg";
+import Responsibility from "../../assets/icons/Frame 16 (2).svg";
+import styles from "./Important.module.scss";
+
+export function Important() {
+  const values = [
+    {
+      img: Quality,
+      title: "Качество",
+      text: `Наши профессионалы работают на \nлучшем оборудовании для пошива \nодежды беспрецедентного \nкачества.`,
+    },
+    {
+      img: Speed,
+      title: "Скорость",
+      text: `Благодаря отлаженной системе в \nWomazing мы можем отшивать до \n20-ти единиц продукции в наших \nсобственных цехах.`,
+    },
+    {
+      img: Responsibility,
+      title: "Ответственность",
+      text: `Мы заботимся о людях и планете \nБезотходное производство и \nкомфортные условия труда — все \nэто Womazing.`,
+    },
+  ];
+  // --------------------------------------------------------------------------
+  return (
+    <section className={styles.important}>
+      <h2>Что для нас важно</h2>
+      <div className={styles.all_content}>
+        {values.map(({ img, title, text }, index) => (
+          <div key={index}>
+            <img className={styles.first_img} src={img} alt={title} />
+            <h3>{title}</h3>
+            <p className={styles.desc}>{text}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Important/index.js
@@ -0,0 +1,1 @@
+export { Important } from "./Important";
