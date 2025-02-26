310e61cca2c1db04436b7050e99acef65cc8cbda
Пользователь <azimazimqp11@gmail.com>
2025-02-26 18:06:12 +0600

    feature: magic in details

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import { ROUTER_PATHS } from "./routesPaths";
 import Layout from "../layouts/Layout";
 import Main from "../pages/Main/Main";
 import Cart from "../pages/Cart/Cart"
+import { Brand } from "../pages/Brand";
 
 export const routes = createBrowserRouter([
   {
@@ -19,7 +20,7 @@ export const routes = createBrowserRouter([
       },
       {
         path: ROUTER_PATHS.brand,
-        element: <p>brand</p>,
+        element: <Brand />,
       },
       {
         path: ROUTER_PATHS.contacts,
