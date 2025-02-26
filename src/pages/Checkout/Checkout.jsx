71ed6eba605ce796577393ce5f8adfb810c72e82
amirlnsrv <amir.lansarov@gmail.com>
2025-02-26 19:37:54 +0600

    feat: added checkout page

---
+++ src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./Checkout.module.scss";
+
+export const Checkout = () => {
+  return (
+    <div className={styles.checkout}>
+      <div className={styles.checkoutLeft}>
+        <div className={styles.checkoutForm}>
+          <h2>Адрес получателя</h2>
+          <form className={styles.addressForm}>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="Страна" />
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="Город" />
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="Улица" />
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="Дом" />
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="Квартира" />
+            </div>
+          </form>
+        </div>
+        <div className={styles.checkoutForm}>
+          <h2>Комментарии</h2>
+          <form className={styles.addressForm}>
+            <div className={styles.inputGroup}>
+              <textarea type="text" placeholder="Страна" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import { ROUTER_PATHS } from "./routesPaths";
 import Layout from "../layouts/Layout";
 import Main from "../pages/Main/Main";
 import Cart from "../pages/Cart/Cart";
+import { Checkout } from "../pages/Checkout";
 
 export const routes = createBrowserRouter([
   {
@@ -28,6 +29,10 @@ export const routes = createBrowserRouter([
       {
         path: ROUTER_PATHS.cart,
         element: <Cart />,
+      },
+      {
+        path: ROUTER_PATHS.checkout,
+        element: <Checkout />,
       },
     ],
   },
