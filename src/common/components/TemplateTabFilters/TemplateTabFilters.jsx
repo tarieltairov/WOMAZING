dba478598bd41615323e94342d20a60494ff459a
Tariel <tarieltairov1@gmail.com>
2025-03-03 21:05:00 +0600

    feat: template tabfilter

---
+++ src/common/components/TemplateTabFilters/TemplateTabFilters.jsx
@@ -0,0 +1,10 @@
+import styles from './TemplateTabFilters.module.scss'
+
+export function TemplateTabFilters({ items = [], active, onChange }) {
+  return (
+    <div className={styles.container}>
+      {/* перебираете items */}
+      {/* onChange(item.value) */}
+    </div>
+  )
+}
