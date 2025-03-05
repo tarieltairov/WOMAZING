8473db5f08d190e43566f914640692ef8736ca96
roza123-coder <kabylovaroza40@gmail.com>
2025-03-05 11:20:44 +0600

    обавлены адаптивные стили для хедера и компонента ProductDetail

--- src/common/components/LogoNav/LogoNav.jsx
+++ src/common/components/LogoNav/LogoNav.jsx
@@ -1,4 +1,42 @@
-import React from 'react'
+// import React from 'react'
+// import { NavLink } from 'react-router-dom'
+// import logo from '../../../assets/icons/logo.svg'
+// import { ROUTER_PATHS } from '../../../routes/routesPaths'
+// import styles from './LogoNav.module.scss'
+// const menuItems = [
+//   { name: 'Главная', path: ROUTER_PATHS.main },
+//   { name: 'Магазин', path: ROUTER_PATHS.shop },
+//   { name: 'О бренде', path: ROUTER_PATHS.brand },
+//   { name: 'Контакты', path: ROUTER_PATHS.contacts },
+// ]
+// export const LogoNav = () => {
+//   return (
+//     <div className={styles.root}>
+//       <div className={styles.logo}>
+//         <img
+//           src={logo}
+//           alt="logo"
+//         />
+//         <div className={styles.logoTitle}>Womazing</div>
+//       </div>
+//       <nav>
+//         <ul className={styles.navigation}>
+//           {menuItems.map((item, index) => (
+//             <li key={index}>
+//               <NavLink
+//                 to={item.path}
+//                 className={({ isActive }) => (isActive ? styles.o : '')}
+//               >
+//                 {item.name}
+//               </NavLink>
+//             </li>
+//           ))}
+//         </ul>
+//       </nav>
+//     </div>
+//   )
+// }
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import logo from '../../../assets/icons/logo.svg'
@@ -14,6 +52,12 @@ const menuItems = [
 ]
 
 export const LogoNav = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen)
+  }
+
   return (
     <div className={styles.root}>
       <div className={styles.logo}>
@@ -25,12 +69,23 @@ export const LogoNav = () => {
       </div>
 
       <nav>
-        <ul className={styles.navigation}>
+        <button
+          className={styles.hamburger}
+          onClick={toggleMenu}
+        >
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </button>
+        <ul
+          className={`${styles.navigation} ${isMenuOpen ? styles.active : ''}`}
+        >
           {menuItems.map((item, index) => (
             <li key={index}>
               <NavLink
                 to={item.path}
                 className={({ isActive }) => (isActive ? styles.o : '')}
+                onClick={() => setMenuOpen(false)} // Закрываем меню при выборе
               >
                 {item.name}
               </NavLink>
