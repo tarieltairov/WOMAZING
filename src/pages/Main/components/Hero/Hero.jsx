46ff4aa77e257361881616b05dcb54e628579412
roza123-coder <kabylovaroza40@gmail.com>
2025-03-06 14:46:29 +0600

    feat: добавлены карточки в мок-данные по категориям

--- src/pages/Main/components/Hero/Hero.jsx
+++ src/pages/Main/components/Hero/Hero.jsx
@@ -5,6 +5,7 @@ import Image2 from 'assets/images/hero_third.jpg'
 
 import { useNavigate } from 'react-router-dom'
 
+// import { ROUTER_PATHS } from '../../../../routes/routesPaths'
 import { ROUTER_PATHS } from 'routes/routesPaths'
 
 import { Button } from 'ui/Button'
