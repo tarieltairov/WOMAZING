aed52b51e8708211a547892b567356615cf53f4a
ravil <zhyndykovravil@gmail.com>
2025-03-07 19:28:32 +0600

    feat: importantSection

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { ImportantSection } from '../src/pages/Main/components/ImportantSection/index.js'
-
 import App from './App.jsx'
-
-// import { ImportantSection } from ''
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <ImportantSection />
+    <App />
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
       ),
       ui: fileURLToPath(new URL('./src/common/ui', import.meta.url)),
       constant: fileURLToPath(new URL('./src/constant', import.meta.url)),
+      routes: fileURLToPath(new URL('./src/routes', import.meta.url)),
     },
   },
 })
